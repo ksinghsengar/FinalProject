ba305a51c44bf20377aaabb97efd3842fb358770
Kamini <kaminisingh1294@gmail.com>
2017-07-14 16:15:49 +0530

    commit

--- src/main/java/com/services/UserServiceImpl.java
+++ src/main/java/com/services/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,6 +15,12 @@ public class UserServiceImpl implements UserService{
     UserDao userDao;
 
     public boolean saveOrUpdateUser(User user) {
+
+        user.setDateCreated(new Date());
+        user.setLastUpdated(new Date());
+        user.setAdmin(false);
+        user.setActive(true);
+
         return userDao.saveOrUpdateUser(user);
     }
 
@@ -21,10 +28,16 @@ public class UserServiceImpl implements UserService{
         return userDao.getUser();
     }
 
-//    public boolean deleteUser(User user) {
-//        return userDao.deleteUser(user);
-//    }*/
- /*public boolean checkPassword(User user){
-     return userDao.checkPassword(user);
- }*/
+    public boolean deleteUser(User user) {
+        return userDao.deleteUser(user);
+    }
+
+     public boolean validateUser(String loginUser, String loginPassword){
+         if(userDao.validateUser(loginUser,loginPassword)){
+
+             return false;
+         }
+         return true;
+     }
+
 }

04646455cc75000f039e00605373bd5662d8208b
Kamini <kaminisingh1294@gmail.com>
2017-07-14 16:13:31 +0530

    commit

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -7,10 +7,7 @@ import com.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Calendar;
@@ -20,39 +17,49 @@ import java.util.List;
 
 @Controller
 public class UserController {
-@Autowired
+    @Autowired
     UserService userService;
 
-        @RequestMapping(value = "/register",method = RequestMethod.POST)
-        public ModelAndView register (@ModelAttribute("User") User user, BindingResult result){
-            ModelAndView modelAndView;
-            if(result.hasErrors()){
-                System.out.println("BINDING Error");
-            }
-            user.setDateCreated(new Date());
-            user.setLastUpdated(new Date());
-            user.setAdmin(false);
-            user.setActive(true);
-            if (userService.saveOrUpdateUser(user)) {
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView register(@ModelAttribute("User") User user, BindingResult result) {
+        ModelAndView modelAndView;
+        if (result.hasErrors()) {
+            System.out.println("BINDING Error");
+        }
+        if (userService.saveOrUpdateUser(user)) {
             modelAndView = new ModelAndView("dashboard");
 
+        } else {
+            modelAndView = new ModelAndView("home");
+
         }
+        return modelAndView;
+
+    }
+
+    @RequestMapping(value = "/getuser")
+    @ResponseBody
+    public ModelAndView getUser() {
+        ModelAndView modelAndView = new ModelAndView("dashboard");
+        System.out.println("users" + userService.getUser());
+        modelAndView.addObject("Users", userService.getUser());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/login")
+    public ModelAndView validateUser(@RequestParam("loginUser")String name,@RequestParam("loginPassword")String password) {
+        ModelAndView modelAndView;
+
+        if (userService.validateUser(name,password))
+            modelAndView = new ModelAndView("dashboard");
+
         else {
             modelAndView = new ModelAndView("home");
 
         }
-            return modelAndView;
-
-    }
-
-    @RequestMapping(value="/getuser")
-   @ResponseBody
-    public  ModelAndView getUser(){
-            ModelAndView modelAndView = new ModelAndView("dashboard");
-        System.out.println("users"+userService.getUser());
-        modelAndView.addObject("Users",userService.getUser());
         return modelAndView;
     }
-    }
+}
+
 
 

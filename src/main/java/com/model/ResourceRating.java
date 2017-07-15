e51092961aed9fa934bbc1a6a13f0dacb0a9668d
Kamini <kaminisingh1294@gmail.com>
2017-07-15 14:01:58 +0530

    commit

--- src/main/java/com/model/ResourceRating.java
+++ src/main/java/com/model/ResourceRating.java
@@ -1,14 +1,59 @@
 package com.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class ResourceRating {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @ManyToOne
+    private Resource resource;
+    @ManyToMany
+    List<User> userList = new ArrayList<>();
+    private Integer score;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "ResourceRating{" +
+                "id=" + id +
+                ", resource=" + resource +
+                ", userList=" + userList +
+                ", score=" + score +
+                '}';
+    }
 }

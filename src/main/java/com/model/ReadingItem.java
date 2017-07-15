7cb6cd0a5503982994004253d935db8a8c2e9254
Kamini <kaminisingh1294@gmail.com>
2017-07-15 12:40:02 +0530

    commit

---
+++ src/main/java/com/model/ReadingItem.java
@@ -0,0 +1,62 @@
+package com.model;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ReadingItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @OneToOne
+    private Resource resource;
+    @ManyToMany
+    List<User> userList = new ArrayList<>();
+    @Type(type = "NumericBooleanType")
+    private  Boolean isRead;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public Boolean getRead() {
+        return isRead;
+    }
+
+    public void setRead(Boolean read) {
+        isRead = read;
+    }
+
+    @Override
+    public String toString() {
+        return "ReadingItem{" +
+                "id=" + id +
+                ", resource=" + resource +
+                ", userList=" + userList +
+                ", isRead=" + isRead +
+                '}';
+    }
+}
